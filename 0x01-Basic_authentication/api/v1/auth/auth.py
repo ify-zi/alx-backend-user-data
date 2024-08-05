512beb9c7a376b663b1a280cbe3d47c809f72add
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-08-05 14:14:12 +0000

    Task #1 to #5: Basic Authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+    Authentication model module
+"""
+
+from flask import request
+from typing import TypeVar, List
+
+
+class Auth:
+    """
+        Class for authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """method definition"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        path = path.rstrip("/")
+        for ex_path in excluded_paths:
+            if ex_path.endswith("*"):
+                ex_path = ex_path[:-1]
+                if path.startswith(ex_path):
+                    return False
+            elif ex_path.rstrip("/") == path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """method definitio"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method definition"""
+        return None
