ee58137c891364e03dcc1e192b738eadd0c87721
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-08-06 08:23:24 +0000

    PEP8 styling correction

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,12 +20,14 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """
         Unauthorized user handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
