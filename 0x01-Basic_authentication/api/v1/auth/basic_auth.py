ec71229993374b1f92956921a66aa4508a158162
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-08-06 11:28:45 +0000

    implemented the the decode function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
     Module for the Basic Authentication
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode as dec
 
 
 class BasicAuth(Auth):
@@ -18,4 +19,18 @@ class BasicAuth(Auth):
             if isinstance(authorization_header, str):
                 if authorization_header.startswith('Basic '):
                     return authorization_header[6:]
+        return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+            Method to conver the base64 encode to str
+        """
+        if base64_authorization_header is not None:
+            if isinstance(base64_authorization_header, str):
+                try:
+                    return dec(base64_authorization_header).decode('utf-8')
+                except Exception:
+                    return None
         return None
