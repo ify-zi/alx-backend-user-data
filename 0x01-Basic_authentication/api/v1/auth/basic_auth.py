8afee5d82d26644b471dc8b8cc27d3747c0d291f
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-08-06 10:07:38 +0000

    Implemnet the BasicAuth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Module for the Basic Authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+        BasicAuth class defintion and its methods
+    """
+    pass
