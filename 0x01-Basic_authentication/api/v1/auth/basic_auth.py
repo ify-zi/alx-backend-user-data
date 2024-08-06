974a22142539cd8251857517f0a7a27872dcc77d
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-08-06 13:11:49 +0000

    implemented the verification process

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@
 from api.v1.auth.auth import Auth
 from base64 import b64decode as dec
 from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -52,17 +53,14 @@ class BasicAuth(Auth):
     def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
         """ retrns the User Object"""
-        if user_email is None or isinstance(user_email, str):
+        if user_email is None or not isinstance(user_email, str):
             return None
-        if user_pwd is None or isinstance(user_pwd, str):
+        if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        try:
-            users = User.search({"email": user_email})
-            if len(users) == 0:
-                return None
-            for user in users:
-                if user.is_valid_password(user_pwd):
-                    return user
+        users = User.search({"email": user_email})
+        if len(users) == 0:
             return None
-        except Exception:
-            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
