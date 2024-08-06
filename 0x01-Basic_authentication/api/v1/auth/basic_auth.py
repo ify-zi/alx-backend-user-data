59d04329fa31e4a2c9c0bd426066dd0ce3ffb433
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-08-06 11:53:35 +0000

    implement the credential extraction from decoded str

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -34,3 +34,16 @@ class BasicAuth(Auth):
                 except Exception:
                     return None
         return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """ Credential Extraction method"""
+        if decoded_base64_authorization_header is not None:
+            if isinstance(decoded_base64_authorization_header, str):
+                s_point = decoded_base64_authorization_header.find(':')
+                if s_point > 0:
+                    username = decoded_base64_authorization_header[:s_point]
+                    cred = decoded_base64_authorization_header[s_point + 1:]
+                    return (username, cred)
+        return (None, None)
