ea627ba77f05f84856f83fa1cb3959ef44f2d3ed
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-08-07 20:43:36 +0000

    created the session maker method in the SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -10,12 +10,10 @@ from api.v1.auth.auth import Auth
 class SessionAuth(Auth):
     """Session auth class definition and methods"""
     user_id_by_session_id = {}
-
-
     def create_session(self, user_id: str = None) -> str:
         """session maker"""
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
-        return self.user_id_by_session_id
+        return session_id
