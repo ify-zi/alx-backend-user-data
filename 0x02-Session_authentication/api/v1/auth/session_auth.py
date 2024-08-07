e72216efdd3e0f3f714ad17fb2bcb487a8db8ee6
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-08-07 20:22:49 +0000

    Starting the implementation of the session_auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Module of the session Authentication
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session auth class definition and methods"""
+    pass
