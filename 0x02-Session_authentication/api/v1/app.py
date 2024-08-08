17bcad9e9e51347664b157170a12931c3c0a13d9
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-08-08 21:05:52 +0000

    Implemented session expiration

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -23,6 +23,9 @@ elif AUTH_TYPE == 'basic_auth':
 elif AUTH_TYPE == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+elif AUTH_TYPE == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
 
 excluded_paths = ['/api/v1/status/',
                   '/api/v1/unauthorized/',
