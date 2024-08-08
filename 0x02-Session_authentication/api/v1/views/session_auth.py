fd88a2c5dc81f4ba60b8642915d6faf934b19bb6
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-08-08 17:03:32 +0000

    fully implemented session auth and cookie

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+    Session authentication views
+"""
+
+import os
+from flask import jsonify, request
+from api.v1.views import app_views
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'],
+                 strict_slashes=False)
+def session_login():
+    """login auth def"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None:
+        return jsonify({"error": "email missing"}), 400
+    if password is None:
+        return jsonify({"error": "password missing"}), 400
+    users = User.search({'email': email})
+    if users is None:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            out = jsonify(user.to_json())
+            out.set_cookie(os.getenv('SESSION_NAME'), session_id)
+            return out
+    return jsonify({"error": "wrong password"}), 401
