adf48e1b9fc82ad5b4ab12c6c3b70996a9558861
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-08-13 06:37:44 +0000

    Update DB model: bug fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,10 +34,9 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """adds user to database"""
-        try:
-            user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
-            self._session.commit()
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
