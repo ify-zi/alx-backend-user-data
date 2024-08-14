edf59aa613ca5c6e151130d3f9ab233cc800f6c5
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-08-14 21:22:55 +0000

    implemented the reset token method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,3 +79,16 @@ class Auth:
             return None
         except Exception:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """returns a reset token for passowrd reset"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                reset_token = str(uuid.uuid4())
+                self._db.update_self(user.id, reset_token=reset_token)
+                return reset_token
+            else:
+                raise ValueError
+        except Exception:
+            pass
